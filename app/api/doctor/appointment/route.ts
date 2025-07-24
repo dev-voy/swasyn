1e009f82e4aa4ff80b14af66594d21b70536c94e
Ashutosh Gaurav <mr.gaurav9554@gmail.com>
2025-07-24 08:32:41 +0530

    feat: add appointment API with GET and POST methods and validation schema

---
+++ app/api/doctor/appointment/route.ts
@@ -0,0 +1,94 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+import { getServerSession } from "next-auth";
+import { ZodError } from "zod";
+import { appointmentSchema } from "@/lib/validations/appointmentValidation";
+
+const session = await getServerSession();
+
+export async function GET() {
+  try {
+    const session = await getServerSession();
+    if (!session || !session.user) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    } else if (session.user.role !== "ADMIN") {
+      return NextResponse.json(
+        { error: "You do not have permission to access this resource." },
+        { status: 403 }
+      );
+    }
+
+    const appointments = await prisma.appointment.findMany();
+
+    return NextResponse.json(
+      {
+        success: true,
+        message: "Appointments fetched successfully",
+        appointments: appointments,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      {
+        success: false,
+        message:
+          error instanceof Error &&
+          `Failed to fetch appointments: ${error.message}`,
+      },
+      { status: 500 }
+    );
+  }
+}
+
+export async function POST(request: NextRequest) {
+  try {
+    if (!session || !session.user) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+    const payload = await appointmentSchema.parseAsync(await request.json());
+    const appointment = await prisma.appointment.create({
+      data: {
+        patientId: (session.user.id as any) || payload.patientId,
+        doctorId: payload.doctorId as any,
+        hospitalId: payload.hospitalId as any,
+        appointmentDate: new Date(payload.appointmentDate),
+        appointmentTime: payload.appointmentTime,
+        appointmentType: payload.appointmentType,
+        appointmentStatus: payload.appointmentStatus,
+        symptoms: payload.symptoms || null,
+        prescription: payload.prescription || null,
+        notes: payload.notes || null,
+      },
+    });
+
+    return NextResponse.json(
+      {
+        success: true,
+        message: "Appointment created successfully",
+        appointment: appointment,
+      },
+      { status: 201 }
+    );
+  } catch (error) {
+    if (error instanceof ZodError) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: error.issues[0].message || "Invalid input data provided",
+        },
+        { status: 400 }
+      );
+    }
+
+    return NextResponse.json(
+      {
+        success: false,
+        message:
+          error instanceof Error &&
+          `Failed to create appointment: ${error.message}`,
+      },
+      { status: 500 }
+    );
+  }
+}
---
+++ lib/validations/appointmentValidation.ts
@@ -0,0 +1,33 @@
+import { z } from "zod";
+
+export const appointmentSchema = z.object({
+  patientId: z.string().cuid2(),
+  doctorId: z.string().cuid2(),
+  hospitalId: z.string().optional(),
+  appointmentDate: z.string().datetime(),
+  appointmentTime: z.string(),
+
+  appointmentType: z
+    .enum([
+      "in_person",
+      "online",
+      "home_visit",
+      "follow_up",
+      "emergency",
+      "virtual",
+    ])
+    .default("in_person"),
+  appointmentStatus: z
+    .enum([
+      "pending",
+      "confirmed",
+      "cancelled",
+      "completed",
+      "no_show",
+      "rescheduled",
+    ])
+    .default("pending"),
+  symptoms: z.string().optional(),
+  prescription: z.string().optional(),
+  notes: z.string().optional(),
+});
